ef57c8cdf7ad5827806b0d0712303d6b57c4e5e7
Luciano Longo <luciano.longo@gmail.com>
2017-10-31 18:58:28 -0300

    Add db:dump command

---
+++ freighter.d/dbdump.sh
@@ -0,0 +1,16 @@
+_fcmd_db:dump() {
+    HELP_USG="[file]"
+    HELP_TXT="Run mysqldump on the $IMG_MYSQL container"
+    _show_help "$@"
+
+    _req_running
+
+    dump=$($COMPOSE exec $IMG_MYSQL mysqldump -u root -p$DB_PASSWORD $DB_DATABASE | grep -v "mysqldump: \[Warning\]")
+
+    if [ ! -z "$1" ]; then
+        echo "$dump" > $1
+        echo "Dump saved to [$1]"
+    else
+        echo "$dump"
+    fi
+}
